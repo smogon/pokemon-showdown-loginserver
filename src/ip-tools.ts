3b8515e814f9c83de1db8047f06fee86ede22c9f
Mia <49593536+mia-pi-git@users.noreply.github.com>
2023-03-26 02:14:24 -0500

    Fix build

--- src/ip-tools.ts
+++ src/ip-tools.ts
@@ -3,6 +3,7 @@
  */
 
 export const IPTools = new class {
+	// eslint-disable-next-line max-len
 	readonly ipRegex = /^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$/;
 	getCidrRange(cidr: string): {minIP: number; maxIP: number} | null {
 		if (!cidr) return null;
