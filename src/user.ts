d13c1a4ee86fe718cfbf51772a25da3e82cf54cf
Mia <49593536+mia-pi-git@users.noreply.github.com>
2023-04-03 09:45:37 -0500

    Fix setting CORS for body sids

--- src/user.ts
+++ src/user.ts
@@ -435,6 +435,9 @@ export class Session {
 
 		// see if we're logged in
 		const scookie = body.sid || this.cookies.get('sid');
+		if (body.sid) {
+			this.context.response.setHeader('Access-Control-Allow-Origin', '*');
+		}
 		if (!scookie) {
 			// nope, not logged in
 			return null;
