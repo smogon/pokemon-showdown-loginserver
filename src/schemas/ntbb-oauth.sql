7d9f49af5185f5600a0b2f5228193563423f72e4
Mia <49593536+mia-pi-git@users.noreply.github.com>
2023-08-14 21:05:47 -0500

    Make client typing more consistent in token table

--- src/schemas/ntbb-oauth.sql
+++ src/schemas/ntbb-oauth.sql
@@ -6,7 +6,7 @@ CREATE TABLE `ntbb_oauth_clients` (
 
 CREATE TABLE `ntbb_oauth_tokens` (
 	owner varchar(18) NOT NULL,
-	client varchar(40) NOT NULL,
+	client varchar(32) NOT NULL,
 	id varchar(32) NOT NULL PRIMARY KEY,
 	time BIGINT(20) NOT NULL
 );
